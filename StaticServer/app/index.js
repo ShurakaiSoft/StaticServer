6966dfef4b84c44930693ef921da263b92d15496
Stephen Parker <shurakaisoft@gmail.com>
2013-06-11 02:28:44 +1000

    Merged server.js into index.js

--- StaticServer/app/index.js
+++ StaticServer/app/index.js
@@ -2,10 +2,29 @@
  * Index file.
  */
 
-
 var port = 1337;
 
-require('../lib/server').start(port, function (err) {
+var staticServer = require('../lib/static-server').init({
+	webroot: '../public',
+	defaultFile: 'test.html'
+});
+	
+require('http').createServer(function (req, res) {
+		
+	staticServer.handleRequest(req, res, function (err) {
+		console.log("Request error: ", err);
+		switch (err) {
+		case 404:
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.end("404, File not Found.");
+			break;
+		default:
+			res.writeHead(500, {"Content-Type": "text/plain"});
+			res.end("500, Internal Server Error.");
+			break;
+		}
+	});
+}).listen(port, function (err) {
 	if (err) {
 		console.log(err);
 	} else {
